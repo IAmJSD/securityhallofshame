39309c36e3fc28d072af689f197f0745d61a85b8
Jake Gealer <jake@gealer.email>
2019-08-24 15:45:16 +0100

    Handle padding

--- frontend/components/shame_base.vue
+++ frontend/components/shame_base.vue
@@ -1,12 +1,11 @@
 <template>
     <div style="padding-left: 20px; padding-right: 20px">
-        <div v-for="(value, key) in d" v-bind:key="key">
+        <div v-for="(value, key) in d" v-bind:key="key" style="padding-bottom: 20px">
             <h1 class="title">{{ key }}</h1>
             <hr style="margin: 5px">
             <dl>
                 <li v-for="(v, k) in value" v-bind:key="k"><a :href="v">{{ k }}</a></li>
             </dl>
-            <hr>
         </div>
     </div>
 </template>
