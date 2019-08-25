9343a352de27e3616217773e7e7af71d9431836a
Jake Gealer <jake@gealer.email>
2019-08-25 11:09:22 +0100

    Fix font/margins

--- frontend/components/shame_base.vue
+++ frontend/components/shame_base.vue
@@ -1,11 +1,12 @@
 <template>
     <div style="padding-left: 20px; padding-right: 20px">
-        <div v-for="(value, key) in d" v-bind:key="key" style="padding-bottom: 20px">
-            <h1 class="title">{{ key }}</h1>
-            <hr style="margin: 5px">
+        <div v-for="(value, key) in d" v-bind:key="key">
+            <h1 class="title is-4">{{ key }}</h1>
+            <hr style="margin: 5px; margin-bottom: 30px">
             <dl>
                 <li v-for="(v, k) in value" v-bind:key="k"><a :href="v">{{ k }}</a></li>
             </dl>
+            <hr style="margin-top: 30px">
         </div>
     </div>
 </template>
